9a160b2f75cf4a396d4833022f989843dcb50695
qtj <qtj.me@outlook.com>
2024-02-05 18:09:51 +0800

    init

---
+++ src/test/java/top/qintj/qinspringboot/QinSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package top.qintj.qinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QinSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
